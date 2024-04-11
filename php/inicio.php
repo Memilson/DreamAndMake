dadbbf1be2b5c6195479a1e764c65e5afb329db5
Angelo <angelolealpl14@gmail.com>
2024-04-11 07:43:21 -0300

    first commit

---
+++ php/inicio.php
@@ -0,0 +1,26 @@
+
+<html>
+
+<head>
+    <link rel="stylesheet" href="../css/geralestilos.css">
+</head>
+<title><?php echo "Bem Vindo" ?></title>
+
+<body>
+<div class="login">
+    <div class="USR">
+        <label for="name" <b>Nome de Usuario</b></label><br>
+        <input type="text" placaholder="Coloque Seu Usuario" name="name" required><br>
+    </div>
+    <div class="PSR">
+        <label for="password" <b>Senha</b></label><br>
+        <input type="password" placeholder="Coloque sua senha" name="password" required><br>
+    </div>
+    <div class="BTN">
+        <button type="submit">Login</button><br>        <a href="url">Esqueceu sua senha?</a>
+    </div>
+    </div>
+
+</body>
+
+</html>
